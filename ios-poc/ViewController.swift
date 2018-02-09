344253c22ba04803cb7080a187c81ccff9508a8e
Bohyung Kim <dsdgun@gmail.com>
2018-02-09 09:38:01 +0900

    Initial Commit

---
+++ ios-poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-poc
+//
+//  Created by bohyung kim on 2018. 2. 9..
+//  Copyright © 2018년 dsdstudio.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
