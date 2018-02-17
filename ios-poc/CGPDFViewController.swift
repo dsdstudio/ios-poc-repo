c85e8163b7a37bf8d9728c5ef61a8b7c15c205c7
Bohyung Kim <dsdgun@gmail.com>
2018-02-18 04:58:34 +0900

    [polishing] sizeToFit pdfview, uipageviewcontroller binding

--- ios-poc/CGPDFViewController.swift
+++ ios-poc/CGPDFViewController.swift
@@ -8,17 +8,122 @@
 
 import UIKit
 
-class CGPDFViewController: UIViewController {
+class CGPDFViewController: UIViewController  {
+    var currentIndex:Int = 0
+    var pageController:UIPageViewController?
+    var doc:CGPDFDocument?
     override func viewDidLoad() {
-        print("pdfviewcontroller")
-        let pdfView = SamplePDFView(frame: view.bounds)
-        pdfView.backgroundColor = .orange
-        view.addSubview(pdfView)
+        pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageController?.delegate = self
+        pageController?.dataSource = self
+        if let pdfUrl:URL = Bundle.main.url(forResource: "sample_pdf", withExtension: ".pdf") {
+            let doc:CGPDFDocument! = CGPDFDocument(pdfUrl as CFURL)
+            self.doc = doc
+            print("document page count :: \(doc.numberOfPages)")
+            let firstController = self.viewController(at: 0)!
+            let controllers = [firstController]
+            pageController?.setViewControllers(controllers, direction: .forward, animated: false, completion: nil)
+            self.addChildViewController(self.pageController!)
+            self.view.addSubview((pageController?.view)!)
+            
+            self.pageController?.view.frame = self.view.bounds
+            pageController?.didMove(toParentViewController: self)
+        }
     }
     
+    func viewController(at index:Int) -> PDFViewControler? {
+        if doc?.numberOfPages == 0 || (doc?.numberOfPages)! <= index {
+            return nil
+        }
+        let vc = storyboard?.instantiateViewController(withIdentifier: "PDFViewControler") as! PDFViewControler
+        vc.pdf = doc
+        vc.pageNumber = index + 1
+        return vc
+    }
+    
+    func indexOfViewController(_ vc:PDFViewControler) -> Int{
+        return vc.pageNumber - 1
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension CGPDFViewController:UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        var index = self.indexOfViewController(viewController as! PDFViewControler)
+        if index == 0 || index == NSNotFound {
+            return nil
+        }
+        
+        index -= 1
+        return self.viewController(at:index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        var index = self.indexOfViewController(viewController as! PDFViewControler)
+        if index == NSNotFound {
+            return nil
+        }
+        
+        index += 1
+        
+        if index == doc?.numberOfPages {
+            return nil
+        }
+        
+        return self.viewController(at:index)
+    }
+}
+
+extension CGPDFViewController:UIPageViewControllerDelegate {
+    
+}
+
+class PDFViewControler: UIViewController {
+    var pdf:CGPDFDocument!
+    var pageNumber:Int = 0
+    override func viewDidLoad() {
+        let v = SamplePDFPageView(frame: view.bounds)
+        v.page = pdf.page(at: pageNumber)
+        v.backgroundColor = .white
+        let rect = v.page?.getBoxRect(.mediaBox)
+        let scale:CGFloat = v.bounds.width / (rect?.width)!
+        v.pdfScale = scale
+        self.view.addSubview(v)
+    }
+}
+
+class SamplePDFPageView:UIView {
+    var page:CGPDFPage? = nil {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    var pdfScale:CGFloat = 1 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()!
+        
+        UIColor.red.setFill()
+        context.saveGState()
+        
+        // 뒤집힌 PDF좌표계로 조정
+        context.translateBy(x: 0.0, y: bounds.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.scaleBy(x: pdfScale, y: pdfScale)
+
+        if let p = self.page {
+            context.drawPDFPage(p)
+        }
+        
+        context.restoreGState()
     }
 }
 
@@ -28,7 +133,6 @@ class SamplePDFView :UIView {
             setNeedsDisplay()
         }
     }
-    
     fileprivate func loadPdfFile() {
         if let pdfUrl:URL = Bundle.main.url(forResource: "arabic", withExtension: ".pdf") {
             let doc:CGPDFDocument! = CGPDFDocument(pdfUrl as CFURL)
@@ -42,7 +146,6 @@ class SamplePDFView :UIView {
                     let pageDictionaryCount = CGPDFDictionaryGetCount(pageDictionary)
                     PDFParser.scanPDF(page: page!)
                     PDFParser.parseAnnotations(pageDictionary: pageDictionary)
-                    print("\(pageIndex) \(page) \(pageDictionary) \(pageDictionaryCount)")
                 }
             }
         }
@@ -52,6 +155,7 @@ class SamplePDFView :UIView {
         super.init(frame:frame)
         loadPdfFile()
     }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
